399ee38530097c15897f9809551beaae2f2c6f14
tienbot1 <tien.ru@mail.ru>
2023-04-04 00:52:58 +0900

    search done

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ import {data as emoji} from './data.js'
 
 function renerCard(data){
     data.forEach(el => createCard(el))
-    // data.forEach(el => checkKeywords(el))
+    data.forEach(el => checkKeywords(el.keywords))
 }
-
 
 function checkKeywords(keyword){
     let words = keyword.toLowerCase().split(' ')
@@ -13,12 +12,14 @@ function checkKeywords(keyword){
         if(!words2.includes(words[i])){
             words2.push(words[i])
         }
-        words2.join('-')
     }
-
-    // console.log(words2)
+    words2 = words2.join(' ')
     return words2
 }
+
+renerCard(emoji)
+
+const card = document.querySelectorAll('.card')
 
 function createCard(obj) {
     const main = document.body.querySelector("section")
@@ -34,10 +35,32 @@ function createCard(obj) {
 
     const p = document.createElement('p')
     p.innerText = checkKeywords(obj.keywords)
-    // p.innerText = obj.keywords
 
     main.append(card)
     card.append(emoji, h2, p)
 }
 
-renerCard(emoji)
+function hideCard(card) {
+    for (let i=0; i<card.length; i++){
+        card[i].style = 'display: none;'
+    }
+}
+
+let input = document.querySelector('input')
+
+input.addEventListener('change', (event) => {
+    hideCard(card)
+    let text = event.target.value
+    for(let i=0; i<card.length; i++){
+        if (card[i].children[1].innerText.toLowerCase().includes(text.toLowerCase()) || card[i].children[2].innerText.toLowerCase().includes(text.toLowerCase())){ 
+            card[i].style="display: flex;"
+        }
+    }
+    event.target.value = ''
+})
+
+let h1 = document.querySelector('h1')
+h1.addEventListener('click', () => {
+    hideCard()
+    renerCard(emoji)
+})
