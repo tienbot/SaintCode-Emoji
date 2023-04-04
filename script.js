13fc1ef62c2151be92fea24dbb604d82c062c699
tienbot1 <tien.ru@mail.ru>
2023-04-05 00:00:07 +0900

    random favicon

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
 import {data as emoji} from './data.js'
 
-function renerCard(data){
-    data.forEach(el => createCard(el))
-    data.forEach(el => checkKeywords(el.keywords))
+renderCard(emoji)
+randomFavicon()
+
+const card = Object.values(document.querySelectorAll('.card'))
+const input = document.querySelector('input')
+const h1 = document.querySelector('h1')
+
+function randomFavicon(){
+    let randomNumb = Math.floor(Math.random() * emoji.length) + 1
+    let randomImg = emoji[randomNumb].symbol
+    let title = document.querySelector('title')
+    title.innerHTML = `Emoji ${randomImg}`
 }
 
-function checkKeywords(keyword){
-    let words = keyword.toLowerCase().split(' ')
-    let words2 = []
-    for(let i=0; i<words.length; i++){
-        if(!words2.includes(words[i])){
-            words2.push(words[i])
+input.addEventListener('change', (event) => {
+    hideCard(card, true)
+    let inputText = event.target.value.toLowerCase()
+    card.map((el) => {
+        let title = el.children[1].innerText.toLowerCase()
+        let keywords = el.children[2].innerText.toLowerCase()
+        if (title.includes(inputText) || keywords.includes(inputText)){ 
+            el.style.display="flex"
         }
-    }
-    words2 = words2.join(' ')
-    return words2
+    })
+    event.target.value = ''
+})
+
+h1.addEventListener('click', () => {
+    hideCard(card, false)
+})
+
+function renderCard(data){
+    data.forEach(el => {
+        el.keywords = delDoubleKeywords(el.keywords)
+        createCard(el)
+    })
 }
 
-renerCard(emoji)
-
-const card = document.querySelectorAll('.card')
+function delDoubleKeywords(keyword){
+    let keywordsWithDoubles = keyword.toLowerCase().split(' ')
+    let keywordsWithoutDoub = []
+    keywordsWithDoubles.map((el) => {
+        if(!keywordsWithoutDoub.includes(el)){
+            keywordsWithoutDoub.push(el)
+        }
+    })
+    keywordsWithoutDoub = keywordsWithoutDoub.join(' ')
+    return keywordsWithoutDoub
+}
 
 function createCard(obj) {
     const main = document.body.querySelector("section")
@@ -34,33 +63,16 @@ function createCard(obj) {
     h2.innerText = obj.title
 
     const p = document.createElement('p')
-    p.innerText = checkKeywords(obj.keywords)
+    
+    p.innerText = obj.keywords
 
     main.append(card)
     card.append(emoji, h2, p)
 }
 
-function hideCard(card) {
-    for (let i=0; i<card.length; i++){
-        card[i].style = 'display: none;'
-    }
+function hideCard(card, isShow) {
+    card.map((el) => {
+        el.style = isShow ? 'display: none;' : 'display: flex;'
+    })
 }
 
-let input = document.querySelector('input')
-
-input.addEventListener('change', (event) => {
-    hideCard(card)
-    let text = event.target.value
-    for(let i=0; i<card.length; i++){
-        if (card[i].children[1].innerText.toLowerCase().includes(text.toLowerCase()) || card[i].children[2].innerText.toLowerCase().includes(text.toLowerCase())){ 
-            card[i].style="display: flex;"
-        }
-    }
-    event.target.value = ''
-})
-
-let h1 = document.querySelector('h1')
-h1.addEventListener('click', () => {
-    hideCard()
-    renerCard(emoji)
-})
